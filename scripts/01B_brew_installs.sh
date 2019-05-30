d2c00f8069dcc74f63e9968d03653ca02f500fa8
Ian Phipps <phippsian@gmail.com>
2019-05-30 17:32:54 -0400

    Add gitignore global and some brew installs

--- scripts/01B_brew_installs.sh
+++ scripts/01B_brew_installs.sh
@@ -5,5 +5,6 @@
 brew update
 brew install python3 rust
 brew install ranger highlight vim
-brew install editorcondig ctags
+brew install editorconfig ctags
 brew install fd ag entr tree
+brew install enhancd

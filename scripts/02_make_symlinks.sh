7433d636b3d0d9df6eae24a2289571cef9a3f224
Ian Phipps <phippsian@gmail.com>
2019-07-16 13:01:00 -0400

    Feature/may updates (#6)
    * Adds folding
    
    * Make cursor blink
    
    * vimmode for terminal
    
    * Add gitignore global and some brew installs
    
    * Sort imports, add python and another theme

--- scripts/02_make_symlinks.sh
+++ scripts/02_make_symlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles/config_files                    # dotfiles directory
 olddir=~/dotfiles_old/config_files             # old dotfiles backup directory
-files="bashrc vimrc git-completion.bash mac_bashrc linux_bashrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc git-completion.bash mac_bashrc linux_bashrc inputrc gitignore_global"    # list of files/folders to symlink in homedir
 
 ##########
 

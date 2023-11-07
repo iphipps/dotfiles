58f5c498c58a946f0dd83f415456174330fd9778
Ian Phipps <phippsian@gmail.com>
2023-11-06 20:02:41 -0500

    Push what's current in my mac config

--- scripts/01B_brew_installs.sh
+++ scripts/01B_brew_installs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ian/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew update
 brew install python3 rust
